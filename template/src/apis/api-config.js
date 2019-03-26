c9d90607786c0d5c20f47051574661de8371a9ae
钟鸣 <zhongming@dankegongyu.com>
2019-03-26 11:22:39 +0800

    增加axios配置

---
+++ template/src/apis/api-config.js
@@ -0,0 +1,20 @@
+const config = {
+  develop: {
+    apiBaseURL: "", // 设置API地址
+    device: "", // 设置设备信息
+    xAppId: 1, // 设置 X-App-Id
+    xDeviceId: "sQedrr4E7LU7zRU5F6aWGjaX9FfjvGogWb8wyHmU", // 设置 X-Device-Id
+  },
+  test: {
+    apiBaseURL: "", // 设置API地址
+    device: "", // 设置设备信息
+    xAppId: "", // 设置 X-App-Id
+    xDeviceId: "", // 设置 X-Device-Id
+  },
+  production: {
+    apiBaseURL: "", // 设置API地址
+    device: "", // 设置设备信息
+    xAppId: undefined,
+    xDeviceId: undefined
+  }
+}
