5911224b48f2c8c9ac9fbe6e0e3dbd5e57846b7b
钟鸣 <zhongming@dankegongyu.com>
2019-03-28 23:45:59 +0800

    update codes

--- meta.js
+++ meta.js
@@ -35,12 +35,12 @@ module.exports = {
   },
 
   prompts: {
-    name: {
-      when: 'isNotTest',
-      type: 'string',
-      required: true,
-      message: 'Project name',
-    },
+    // name: {
+    //   when: 'isNotTest',
+    //   type: 'string',
+    //   required: true,
+    //   message: 'Project name',
+    // },
     // description: {
     //   when: 'isNotTest',
     //   type: 'string',
@@ -48,11 +48,11 @@ module.exports = {
     //   message: 'Project description',
     //   default: 'A Vue.js project',
     // },
-    author: {
-      when: 'isNotTest',
-      type: 'string',
-      message: 'Author',
-    },
+    // author: {
+    //   when: 'isNotTest',
+    //   type: 'string',
+    //   message: 'Author',
+    // },
     // build: {
     //   when: 'isNotTest',
     //   type: 'list',
