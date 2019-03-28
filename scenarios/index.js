34fc58de3f7d609fcd51959fa4c69c8b78762072
钟鸣 <zhongming@dankegongyu.com>
2019-03-28 23:46:29 +0800

    update codes

--- scenarios/index.js
+++ scenarios/index.js
@@ -10,10 +10,6 @@ const isTest = exports.isTest = index !== -1
 
 const scenario = isTest && require(`./${scenarios[index]}.json`)
 
-console.log(' INDEX in SCENARIOS: ', index)
-console.log(' isTest: ', isTest)
-console.log(' scenario: ', scenario)
-
 exports.addTestAnswers = (metalsmith, options, helpers) => {
   Object.assign(
     metalsmith.metadata(), {
