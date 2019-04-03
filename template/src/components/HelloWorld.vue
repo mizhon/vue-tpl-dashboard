cb09c2903a76cb92407338622cd0526e3b14b7e5
mizhon <mizhon@live.cn>
2019-04-03 11:56:51 +0800

    update codes

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -6,18 +6,19 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to the dashboard Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
