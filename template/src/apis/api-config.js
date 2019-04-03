fbd9c82ae5fd9c524d06fcdfb414ba52df229693
mizhon <mizhon@live.cn>
2019-04-03 13:04:35 +0800

    avoid comma-dangle

--- template/src/apis/api-config.js
+++ template/src/apis/api-config.js
@@ -3,13 +3,13 @@ const config = {
     apiBaseURL: "", // 设置API地址
     device: "", // 设置设备信息
     xAppId: 1, // 设置 X-App-Id
-    xDeviceId: "sQedrr4E7LU7zRU5F6aWGjaX9FfjvGogWb8wyHmU", // 设置 X-Device-Id
+    xDeviceId: "sQedrr4E7LU7zRU5F6aWGjaX9FfjvGogWb8wyHmU" // 设置 X-Device-Id
   },
   test: {
     apiBaseURL: "", // 设置API地址
     device: "", // 设置设备信息
     xAppId: "", // 设置 X-App-Id
-    xDeviceId: "", // 设置 X-Device-Id
+    xDeviceId: "" // 设置 X-Device-Id
   },
   production: {
     apiBaseURL: "", // 设置API地址
