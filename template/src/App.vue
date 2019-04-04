7baea1d48b2e854beb72269127a2f92b76c5d78e
mizhon <mizhon@live.cn>
2019-04-04 19:18:52 +0800

    update codes

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/imgs/logo.png"> -->
     <router-view/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld";
-
 export default {
-  name: "App",
-  // components: {
-  //   HelloWorld
-  // }
-};
+  name: "App"
+}
 </script>
 
 <style lang="less">
-// #app {
-//   font-family: "Avenir", Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
 </style>
