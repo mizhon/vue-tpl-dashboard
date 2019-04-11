8cb5138f12290776bf4687fb2204d72b0344e693
mizhon <mizhon@live.cn>
2019-04-12 03:51:39 +0800

    update codes

--- template/config/prod.env.js
+++ template/config/prod.env.js
@@ -1,5 +1,4 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
-  BASE_URL: '/prod-api'
+  NODE_ENV: '"production"'
 }
--- template/config/test.env.js
+++ template/config/test.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  BASE_URL: '/test-api'
+  NODE_ENV: '"testing"'
 })
