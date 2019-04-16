bb621de4a59c3b4dad9043357e558f5a08131e4b
mizhon <mizhon@live.cn>
2019-04-16 13:56:12 +0800

    comment out request.js console codes

--- template/src/utils/request.js
+++ template/src/utils/request.js
@@ -18,7 +18,7 @@ service.interceptors.request.use(
       // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
       config.headers['X-Token'] = getToken()
     }
-    console.log('[<<< Request.js config settings: >>>]', config)
+    // console.log('[<<< Request.js config settings: >>>]', config)
     return config
   },
   err => {
