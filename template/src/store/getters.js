fdd7fc8cc0b91d6183506785ad0d19b469b46f75
yinhang <yinhang@dankegongyu.com>
2019-04-17 10:30:07 +0800

    fix getters

--- template/src/store/getters.js
+++ template/src/store/getters.js
@@ -8,7 +8,7 @@ const getters = {
   avatar: state => state.user.avatar,
   name: state => state.user.name,
   roles: state => state.user.roles,
-  permission_routes: state => state.permission.routes,
+  permissionRoutes: state => state.permission.routes,
   addRoutes: state => state.permission.addRoutes
 }
 export default getters
