7e55be88f5ca9f75182f8e64574d0c3c57abfeba
mizhon <mizhon@live.cn>
2019-04-27 09:15:49 +0800

    update sso codes, change typo

--- template/src/utils/auth.js
+++ template/src/utils/auth.js
@@ -1,15 +1,42 @@
 import Cookies from 'js-cookie'
 
-const TOKEN_KEY = 'Admin-Token'
+const TOKEN_KEY = 'token'
+
+export function getUser() {
+  let user = localStorage.getItem('USER')
+  if (user) {
+    return JSON.parse(user)
+  } else {
+    return null
+  }
+}
+
+export function setUser(user) {
+  console.log('[@utils/auth.js] --- 保存用户信息: ', user)
+  localStorage.setItem('USER', JSON.stringify(user))
+}
 
 export function getToken() {
-  return Cookies.get(TOKEN_KEY)
+  const token = Cookies.get(TOKEN_KEY)
+  console.log('[util/auth.js] --- 获取token: ', token)
+  if (token !== '' || token !== undefined || token !== null) {
+    return token
+  } else {
+    return null
+  }
 }
 
 export function setToken(token) {
-  return Cookies.set(TOKEN_KEY, token)
+  console.log('[utils/auth.js] ---> ', token)
+  return Cookies.set(TOKEN_KEY, token, {
+    expires: 1
+  })
 }
 
 export function removeToken() {
+  return Cookies.remove(TOKEN_KEY)
+}
+
+export function removeUser() {
   return Cookies.remove(TOKEN_KEY)
 }
--- template/src/utils/sso.js
+++ template/src/utils/sso.js
@@ -5,14 +5,15 @@ import config from '../../config/index'
  */
 
 // 需配置clientId
-const clinetId = config.clinetId
+const clientId = config.clientId
 const ssoURL = process.env.NODE_ENV === 'testing'
   ? config.dev.SSO_BASE_URL
   : config.prod.SSO_BASE_URL
 
 export const randomString = len => {
   let rt = ''
-  let arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  let arr = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
   ]
@@ -27,8 +28,8 @@ export const randomString = len => {
 export const getSSOLoginURL = (to = null) => {
   return ssoURL + 'staff/login?' + [
     'state=' + randomString(32),
-    'client_id' + clinetId,
-    'redirect_url=' + encodeURIComponent(location.origin + '/login' + (to ? '?redirect=' + to.fullPath : ''))
+    'client_id=' + clinetId,
+    'redirect_uri=' + encodeURIComponent(location.origin + '/login' + (to ? '?redirect=' + to.fullPath : ''))
   ].join('&')
 }
 
